e50570f4e7d82e243467921aa34cebff21954cc9
coderhisham <imhish4m@gmail.com>
2024-06-18 23:18:56 +0530

    Updated

--- Web-Development-Practice/Backend/MongoosewithPlainJS/07.findandupdate.js
+++ Web-Development-Practice/Backend/MongoosewithPlainJS/07.findandupdate.js
@@ -22,14 +22,10 @@ const userSchema = new mongoose.Schema({
 
 //Creating a Model and Collection
 const User /*Model Name*/ = mongoose.model("User", userSchema);
-
 /*************************************************************************************************************************************/
 
-//UpdateOne
-User.updateOne(
-  { name: "Hisham" },
-  { name: "Hisham", email: "Hisssham@yahoo.com" }
-)
+//Find One and Update
+User.findOneAndUpdate({ age: { $gt: 15 } }, { age: 42 }) //Here new is false by default
   .then((res) => {
     console.log(res);
   })
@@ -37,7 +33,26 @@ User.updateOne(
     console.log(err);
   });
 
-User.updateMany({ age: { $gt: 15 } }, { age: 88 })
+/* By default this method returns the old document so we need to modify that with another parameter..*/
+User.findOneAndUpdate({ age: { $gt: 15 } }, { age: 55 }, { new: true })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Find By ID and Update
+User.findByIdAndUpdate("666f32ec18e1b38cc91530c7", { age: 16 }) //Here new is false by default
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+/* By default this method returns the old document so we need to modify that with another parameter..*/
+User.findByIdAndUpdate("666f32ec18e1b38cc91530c6", { age: 20 }, { new: true })
   .then((res) => {
     console.log(res);
   })
