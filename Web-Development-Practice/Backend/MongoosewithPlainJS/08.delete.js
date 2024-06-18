340daee397eb9dca023e85eba6d2aae4703e828c
coderhisham <imhish4m@gmail.com>
2024-06-18 23:19:46 +0530

    Updated

---
+++ Web-Development-Practice/Backend/MongoosewithPlainJS/08.delete.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+//Connecting
+connectDB()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(`Connection failed with error: ${err}`);
+  });
+
+async function connectDB() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/mongooseintegration");
+}
+
+//defining Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
