17d84ddc7082127e2dbb16e7aa658e47d19886b6
coderhisham <imhish4m@gmail.com>
2024-07-02 15:45:54 +0530

    Updated

--- Web-Development-Practice/React/Restart/03tailwind_props/src/App.jsx
+++ Web-Development-Practice/React/Restart/03tailwind_props/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl bg-green-500 rounded-md p-5">
+        Vite with Tailwind
+      </h1>
+      <Card
+        userName="Hisham"
+        Designation="CEO"
+        imageUrl="https://avatars.githubusercontent.com/u/84192623?v=4"
+      />
+      <Card
+        userName="Anonymous"
+        Designation="unknown"
+        imageUrl="https://play-lh.googleusercontent.com/ai7FC9zp1bG8zLcl97w9rNde_oZ5s086XP1ZkBFdwf72d_owIiUVJu1-XNp6eOO-AGg"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
