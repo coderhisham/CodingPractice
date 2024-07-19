41689858b21b779ccc690d26a702c24116c7cc5a
coderhisham <imhish4m@gmail.com>
2024-07-19 11:25:11 +0530

    Updated

--- Web-Development-Practice/React/Restart/06CustomHooks/src/components/inputBox/InputBox.jsx
+++ Web-Development-Practice/React/Restart/06CustomHooks/src/components/inputBox/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useId } from "react";
 
 function InputBox({
   label,
@@ -11,17 +11,40 @@ function InputBox({
   currencyDisabled = false,
   className = "",
 }) {
+  const id = useId();
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1-2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
         <input
-            type="Number",
-            value={amount},
-            disabled={amountDisabled},
-            
+          id={id}
+          type="number"
+          value={amount}
+          disabled={amountDisabled}
+          placeholder="Amount"
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+          className="outline-none w-full bg-transparent py-1.5"
         />
       </div>
+      <div className="w-1-2 flex flex-wrap justify-end text-right" />
+      <p className="text-black/40 mb-2 w-full">Currency Type</p>
+      <select
+        className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+        value={selectedCurrency}
+        onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        disabled={currencyDisabled}
+      >
+        {currencyOptions.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
