f0c3cc36b0d2411f2cdc04378adfe08cc2ada716
coderhisham <imhish4m@gmail.com>
2025-01-07 07:56:06 +0530

    Nest js: Providers and Dependency Injection

---
+++ Web-Development-Practice/nestjs/03providers/src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Post() // POST /users
+  create(
+    @Body()
+    userData: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(userData);
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(
+    @Param(':id') id: string,
+    @Body()
+    updatedData: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, updatedData);
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(+id);
+  }
+}
